879dde96502b65ddb3022881f8dc8e2342b4b928
Azzahra Dinda <azzahradindas@gmail.com>
2020-02-15 17:40:06 +0700

    UPDATE - Update Intent and Spinner.

--- app/src/main/java/id/putraprima/marketplacelayout/ProfileActivity.java
+++ app/src/main/java/id/putraprima/marketplacelayout/ProfileActivity.java
@@ -2,7 +2,9 @@ package id.putraprima.marketplacelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void back(View view) {
+        finish();
+    }
+
+    public void edit(View view) {
+        Intent i = new Intent(this, RegisterActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/id/putraprima/marketplacelayout/RegisterActivity.java
+++ app/src/main/java/id/putraprima/marketplacelayout/RegisterActivity.java
@@ -2,7 +2,9 @@ package id.putraprima.marketplacelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void back(View view) {
+        finish();
+    }
+
+    public void next(View view) {
+        Intent i = new Intent(this, ProfileActivity.class);
+        startActivity(i);
     }
 }
