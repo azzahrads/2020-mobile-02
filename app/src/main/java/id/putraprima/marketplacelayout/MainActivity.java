871e686285cc7fff95acca1cc6c823517b748523
Azzahra Dinda <azzahradindas@gmail.com>
2020-02-15 16:08:45 +0700

    UPDATE - Renamed to Login Activity

--- app/src/main/java/id/putraprima/marketplacelayout/MainActivity.java
+++ app/src/main/java/id/putraprima/marketplacelayout/MainActivity.java
@@ -2,7 +2,9 @@ package id.putraprima.marketplacelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void user(View view){
+        Intent i = new Intent(this,RegisterActivity.class);
+        startActivity(i);
+    }
+
+    public void sprovider(View view) {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+    }
+
+    public void guest(View view) {
+        Intent i = new Intent(this,ProfileActivity.class);
+        startActivity(i);
     }
 }
